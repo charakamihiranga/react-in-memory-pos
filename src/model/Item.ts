85750f8f510a25b1a08088df6ae44fe6cb595eba
Charaka V Mihiranga <charakavmihiranga@gmail.com>
2024-12-30 22:54:05 +0530

    add Customer and Item model classes

---
+++ src/model/Item.ts
@@ -0,0 +1,12 @@
+export class Item {
+    code: string;
+    name: string;
+    price: number;
+    quantity: number;
+    constructor(code: string, name: string, price: number, quantity: number) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
